0774036e843dbdc1c999c74bc12df11e55f616f1
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-25 10:14:09 +0000

    save

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,1 +1,14 @@
-console.log("This time run is neceassary")
+// const IdNumber = 95532;
+var AccountId = "sajjadchandio@gmail.com";
+let AccounterCity = "Quetta";
+AccountantPlace = "Shop";
+
+const IdNumber = 576
+var AccountId  =  "Ahmeddbillal" //Globlly Scope Object
+const  Account =
+
+// console.table([IdNumber,AccountId,AccounterCity,AccountantPlace])
+console.log({IdNumber,AccountId,AccounterCity,AccountantPlace})
+// console.log(xyz)
+
+
