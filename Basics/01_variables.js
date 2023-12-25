f31b2313795542e8c2718e2b91f8cca34d5a8a95
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-25 13:08:02 +0000

    save

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,14 +1,27 @@
 // const IdNumber = 95532;
 var AccountId = "sajjadchandio@gmail.com";
-let AccounterCity = "Quetta";
+var AccounterCity = "Quetta";
 AccountantPlace = "Shop";
 
 const IdNumber = 576
 var AccountId  =  "Ahmeddbillal" //Globlly Scope Object
-const  Account =
+var AccounterCity  = "Mastung"
 
-// console.table([IdNumber,AccountId,AccounterCity,AccountantPlace])
+/* when value is not known?
+var Account =
+var Account;
+These are two mathods where I Stock, mean comparisan in these two are really tiny(little).
+JavaScript Identifiers let, var and const when use in a first way |Acount = | Undifined is throw in the terminal if we Execute it, also not error throw if not execute.
+In another way |Account; | if we write, only can declared by  except const then undifiend you will see in the terminal, const constant tell you Missing initializer in const declaration.
+*/
+/*
+ 1. When value is known only we use var, because it is Globelly Scope Identifier. Morever for that issue let and const is came.
+ 2. Variable or Constant is not use for same value, and for same value not using the different declaration except var.
+ 3. A lot of confusion is created when we use var , So by var declaration is ignored, it is not a bad idea.
+ 4. Best technique is avoid same declaration , and take the some meaningfull identifiers and should be change from rest of the identifiers 
+ */
 console.log({IdNumber,AccountId,AccounterCity,AccountantPlace})
-// console.log(xyz)
+// console.table([IdNumber,AccountId,AccounterCity,AccountantPlace])
+// console.log(Account)
 
 
