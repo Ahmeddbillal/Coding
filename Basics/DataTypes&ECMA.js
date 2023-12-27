a9c862d89510fa0ff734a2cf63320dd578d741fa
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-27 16:49:10 +0000

    DataType&ECMA

--- Basics/DataTypes&ECMA.js
+++ Basics/DataTypes&ECMA.js
@@ -1,4 +1,4 @@
-"Use Strict" // Conversion to  a new version of JavaScript
+// "Use Strict"  Conversion to  a new version of JavaScript
 /*
  1. The above Statment will convert your standared of javascript with the new version
  of Javascript,
@@ -10,3 +10,33 @@ features is working.
  alone in a browser, after Ryan Dahl Shifted into C++ Purpopse to executenot
  only in a browser but in a Node.js which is used in many runtimes.
  */
+/*
+Data Types ?
+There are 7  Data types in Java Script
+String => Charaters manipulate and represents.
+itself it is a object.
+
+Number => store a number by 2 power of 53. Floating
+-point like 37 or 1.32
+
+begInt => store a big number. like in stock 
+exchange.Charaters
+
+Boolean => is worked on Condition is true or false.
+
+Null => is an empty value which my js teacher says:
+Standalone value.
+
+Undefined => Simply mean which is not Defined not 
+null (empty) empty is also execute. but not undefined.
+
+Object =>
+*/
+let age = 12; // Number
+let price = 999570; // begInt
+let name = "bilal" //String
+let isLoggedIn = false //boolean
+let empty = null //by the name implies it
+let notSure = undefined // by the name it implies
+
+console.log(name)
