0dee2353ae52a1dcc5dde6cbd05760e56fe3f9db
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-27 14:00:18 +0000

    Ecma and Data types

---
+++ Basics/DataTypes&ECMA.js
@@ -0,0 +1,12 @@
+"Use Strict" // Conversion to  a new version of JavaScript
+/*
+ 1. The above Statment will convert your standared of javascript with the new version
+ of Javascript,
+ 2. Before when "ECMA Script" is not developed there is no standared 
+ for Javascript developers. Solving this issue Javascript Committee introduce
+"ECMA Script".It is now a standared for Javascript where eveyone new develope
+features is working.
+ 3. As we know thet  the  V8 engine of browser is no more carry the engine
+ alone in a browser, after Ryan Dahl Shifted into C++ Purpopse to executenot
+ only in a browser but in a Node.js which is used in many runtimes.
+ */
