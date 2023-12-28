98acb3d860c2b8748588b4fdb8fa4b46d03a8f1b
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-28 14:12:33 +0000

    conversion

---
+++ Basics/03_OnlyConversion.js
@@ -0,0 +1,24 @@
+/* This talk is about Conversion?
+Whats did some times data came from the Server
+and we don't know, What data types is. Takeover
+that knowledge we have a conversion in Java
+Script.Actually it just a one reason some more
+reasons will known soon. 
+*/
+let trues = 1;
+let fuse = 0;
+let score = 90;
+let Runrate = "100";
+let noun = "Melburn"; 
+let numberConversionTo = String(score)
+console.log(typeof numberConversionTo)// String Out-Put is 90
+
+let stringConversionTo = Number(Runrate)
+console.log(stringConversionTo)
+console.log(typeof stringConversionTo)
+console.log(Runrate)
+
+let ConversionTo = Boolean(trues)
+console.log(ConversionTo) 
+console.log(typeof ConversionTo) 
+
