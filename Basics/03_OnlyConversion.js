64a4dca20068e1dfa57cd7684f0c5307c05ac871
Ahmeddbillal <154672339+Ahmeddbillal@users.noreply.github.com>
2023-12-31 15:53:09 +0000

    Stack & Heap

--- Basics/03_OnlyConversion.js
+++ Basics/03_OnlyConversion.js
@@ -5,6 +5,9 @@ that knowledge we have a conversion in Java
 Script.Actually it just a one reason some more
 reasons will known soon. 
 */
+/*
+if there is some fault in a value, then we convert into a number, out put is NaN Not a Number.Example 12dds, undifined , mean thease value not convert to a number it have to be give result NaN.
+*/
 let trues = 1;
 let fuse = 0;
 let score = 90;
@@ -22,3 +25,29 @@ let ConversionTo = Boolean(trues)
 console.log(ConversionTo) 
 console.log(typeof ConversionTo) 
 
+
+/*****************  Operations  *****************/
+// Different method of combining values.
+let value = 6;
+let negValue = -value
+console.log(negValue)
+
+// new method of combining valuse.
+let str1 = "Paining in";
+let str2 = " head";
+let str3 = str1 + str2;
+console.log(str3);
+
+// Combining string & number, importatnt this is look it sides.
+let num1 = 12 + "11";
+let num2 = "11" + 12;
+let num3 = 11 + 12 + "33";
+let num4 = "44" + 11 + 12;
+ console.log([num1,num2,num3,num4])
+ 
+
+ let myobj = {
+    name: "bilal",
+    age: 20,
+}
+console.log(myobj)
---
+++ Basics/04_DataTypes.js
@@ -0,0 +1,48 @@
+
+/*Data Types:-
+1. We Using Data types in javascript to store 
+data in Memory and Access.
+2. In Javascript we Built  the application and
+use it in a many ways. For these porpuses 
+we have some data types in JavaScript.
+We will See Below.
+There are two types of Data types.
+1.Primitive Data Types(call by value)
+2.Non-Primitive Data Types (call by Reference)
+
+Primitive Data types are 7 which is below:
+1.String (number)
+2.Number (number)
+3.BigInt (bigInt  with small n attached in last)
+4.Boolean (True & False)
+5.Symbol (Known as Unique typeof is Symbol)
+6.Null (object)
+7.Undifiend(undifiend)
+
+Non-Primitive Data types are 3 which is below:
+1.Array    (object)
+2.Object   (object)
+3.Function (Object Function says, I Function sees?)
+*/
+//It is a Object
+// let myobj = {
+//     name: "bilal",
+//     age: 20,
+// }
+// console.log(typeof myobj)
+
+//It is a Function
+//  let func1 = function(){
+//      console.log("Error should be not come")
+// }
+// console.log(typeof func1)
+
+// It is a Array
+// let jack = [22,11,12,31]
+// console.log(typeof jack)
+
+let hollow; 
+
+
+console.log(typeof hollow)
+// Symbol()
